6beffe46bcace7c6942f0f65ea4c4068c7c03283
Lipskii <46229315+Lipskii@users.noreply.github.com>
2020-07-15 15:12:32 +0200

    updated BuchalkaCourse

--- src/BuchalkaCourse/TodoData_Singleton.java
+++ src/BuchalkaCourse/TodoData_Singleton.java
@@ -10,7 +10,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Iterator;
 import java.util.List;
 
 public class TodoData_Singleton {
