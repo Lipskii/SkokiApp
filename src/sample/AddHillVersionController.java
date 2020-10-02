2936cf519ae3dba82ec37b2e608198e498b86de3
Lipskii <46229315+Lipskii@users.noreply.github.com>
2020-10-02 13:39:38 +0200

    Update to AddHillVersionController.java layout

--- src/sample/AddHillVersionController.java
+++ src/sample/AddHillVersionController.java
@@ -4,17 +4,14 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 
 
 public class AddHillVersionController {
 
     DataSource dataSource;
     Hill selectedHill;
-    Group group;
+    // Group group;
 
     @FXML
     Button selectButton;
@@ -23,19 +20,63 @@ public class AddHillVersionController {
     @FXML
     ComboBox<TypeOfHill> typeOfHillComboBox;
     @FXML
+    Label existingVersionsLabel;
+    @FXML
+    Label firstYearOfThisVersionLabel;
+    @FXML
+    Label hillSizeLabel;
+    @FXML
+    Label hillVersionRecordLabel;
+    @FXML
+    Label inrunAngleLabel;
+    @FXML
+    Label inrunLengthLabel;
+    @FXML
+    Label kPointLabel;
+    @FXML
+    Label lastYearOfThisVersionLabel;
+    @FXML
     Label selectionLabel;
+    @FXML
+    Label takeOffAngleLabel;
+    @FXML
+    Label takeOfHeightLabel;
+    @FXML
+    Label takeOffLengthLabel;
     @FXML
     Label typeOfHillLabel;
     @FXML
     ListView<Hill> hillListView;
+    @FXML
+    ListView<HillVersion> hillVersionListView;
+    @FXML
+    TextField firstYearTextField;
+    @FXML
+    TextField hillSizeTextField;
+    @FXML
+    TextField hillVersionRecordTextField;
+    @FXML
+    TextField inrunAngleTextField;
+    @FXML
+    TextField inrunLengthTextField;
+    @FXML
+    TextField kPointTextField;
+    @FXML
+    TextField lastYearTextField;
+    @FXML
+    TextField takeOffAngleTextField;
+    @FXML
+    TextField takeOffHeightTextField;
+    @FXML
+    TextField takeOffLengthTextField;
 
     public void initialize() {
         dataSource = new DataSource();
 
-        group = new Group();
+        // group = new Group();
 
-        group.getChildren().addAll(typeOfHillComboBox, typeOfHillLabel);
-        group.setVisible(false);
+        //  group.getChildren().addAll(typeOfHillComboBox, typeOfHillLabel);
+
 
         refresh();
 
@@ -50,14 +91,15 @@ public class AddHillVersionController {
     public void handleSelectButton() {
         if (selectedHill != null) {
             selectionLabel.setText("You've selected " + selectedHill);
-            group.setVisible(true);
-            System.out.println("DONE");
+            //      group.setVisible(true);
         } else
             selectionLabel.setText("Choose a hill to proceed");
     }
 
 
     public void refresh() {
+        //  group.setVisible(false);
+
         countryComboBox.setItems(dataSource.getCountryList());
         countryComboBox.valueProperty().addListener(((observableValue, country, t1) -> hillListView.setItems(dataSource.getHillByCountry(t1))));
 
